e2564d88d525aedf3d25beb6453761803a0b5668
Jagadeesh Inturu <jagadeesh.inturu26@gmail.com>
2026-02-20 23:22:39 +0530

    terraform changes

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -6,4 +6,33 @@ resource "aws_instance" "example" {
     Name = "terraform"
     Project= "aicloudera"
   }
+} 
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow-all-terraform" # this is for AWS account
+  description = "Allow TLS inbound traffic and all outbound traffic"
+ 
+
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  tags = {
+    name        = "allow-all-terraform" 
+    Project= "aicloudera"
+  }
 }
